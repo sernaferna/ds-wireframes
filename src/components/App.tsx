d46338f0a5c4a9e0d07ece3f47d06b13cd45073d
sernaferna <sernaferna@gmail.com>
2021-12-01 12:59:22 -0500

    removed unused import

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Header } from './common/header';
 import { Footer } from './common/footer';
 import { SizeIndicator } from './common/SizeIndicator';
