5f5f56e9eb5c8c8ecb110a83d09547cde2a5ad07
sernaferna <sernaferna@gmail.com>
2022-01-12 12:01:25 -0500

    adjusted styling for list of notes for a given passage

--- apps/ds-ui/src/components/read/notes/NotesSnippet.tsx
+++ apps/ds-ui/src/components/read/notes/NotesSnippet.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useGetNoteByIdQuery } from '../../../services/VapiService';
 import { LoadingMessage, ErrorLoadingDataMessage } from '../../common/loading';
 import MDEditor from '@uiw/react-md-editor';
-import { useDispatch } from 'react-redux';
-import { updateSelectedNote } from '../../../stores/UISlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateSelectedNote, getSelectedNote } from '../../../stores/UISlice';
 
 interface NotesSnippetInterface {
   noteID: string;
 }
 export const NotesSnippet = ({ noteID }: NotesSnippetInterface) => {
+  const selectedNote = useSelector(getSelectedNote);
   const { data, error, isLoading } = useGetNoteByIdQuery(noteID);
   const dispatch = useDispatch();
 
@@ -19,16 +20,18 @@ export const NotesSnippet = ({ noteID }: NotesSnippetInterface) => {
     return <ErrorLoadingDataMessage />;
   }
 
-  const noteSnippet = data!.text.length > 99 ? data!.text.substring(0, 99) + '...' : data!.text;
+  const textToDisplay = noteID === selectedNote ? '[Editing] ' + data!.text : data!.text;
+  const noteSnippet = textToDisplay.length > 99 ? textToDisplay.substring(0, 99) + '...' : textToDisplay;
 
   const selectNote = () => {
-    console.log(`selectNote for ${noteID}`);
-    dispatch(updateSelectedNote(noteID));
+    if (noteID !== selectedNote) {
+      dispatch(updateSelectedNote(noteID));
+    }
   };
 
   return (
     <div
-      className="p-2 bg-light my-2 border border-2"
+      className={noteID === selectedNote ? 'note-snippet-selected' : 'note-snippet'}
       onClick={() => {
         selectNote();
       }}
