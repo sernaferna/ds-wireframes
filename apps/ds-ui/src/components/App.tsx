884b3cb8dec46ec84bfb624024f51a8d4ac259ef
sernaferna <sernaferna@gmail.com>
2022-01-29 16:29:18 -0500

    scaffolding for editing plan

--- apps/ds-ui/src/components/App.tsx
+++ apps/ds-ui/src/components/App.tsx
@@ -11,6 +11,7 @@ import { FourOhFour } from './common/FourOhFour';
 import ToastContainer from 'react-bootstrap/ToastContainer';
 import { PrayerPage } from './prayer/PrayerPage';
 import { PlansPage } from './plans/PlansPage';
+import { EditPlan } from './plans/edit/EditPlan';
 
 export function App() {
   return (
@@ -24,6 +25,7 @@ export function App() {
             <Route key="/read" path="/read" element={<ReadPage />} />
             <Route key="/do" path="/do" element={<DoPage />} />
             <Route key="/plans" path="/plans" element={<PlansPage />} />
+            <Route key="/plans/edit" path="/plans/edit" element={<EditPlan />} />
             <Route key="/help" path="/help" element={<Help />} />
             <Route path="*" element={<FourOhFour />} />
           </Routes>
---
+++ apps/ds-ui/src/components/plans/edit/EditPlan.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EditPlan = () => {
+  return <h1>Edit Plan</h1>;
+};
