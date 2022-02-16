49c347cabe5eca4a6fe729ee9c9739ed808e8863
sernaferna <sernaferna@gmail.com>
2022-02-16 15:00:06 -0500

    added some extra tests

--- packages/refparse/src/__test__/getFormatted.test.ts
+++ packages/refparse/src/__test__/getFormatted.test.ts
@@ -26,4 +26,16 @@ describe('get formatted test suite', () => {
     formatted = getFormattedReference('Genesis 1:1-1:2');
     expect(formatted).toEqual('Genesis 1:1–2');
   });
+
+  it('converts multiple comma-separated into single ref', () => {
+    const formatted = getFormattedReference('Gen.1.1,Gen.1.2');
+
+    expect(formatted).toEqual('Genesis 1:1–2');
+  });
+
+  it('handles a complex set of ranges and commas', () => {
+    const formatted = getFormattedReference('Gen.1.1, Gen.1.2–Gen.1.3, Gen.1.4-5');
+
+    expect(formatted).toEqual('Genesis 1:1–5');
+  });
 });
