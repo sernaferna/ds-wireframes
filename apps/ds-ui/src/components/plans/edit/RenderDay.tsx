1fa84f77747a80e5cfd50ef3e2e3b438a2a58ca8
sernaferna <sernaferna@gmail.com>
2022-02-21 12:56:10 -0500

    new helper control

---
+++ apps/ds-ui/src/components/plans/edit/RenderDay.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { CaretUpFill, CaretDownFill } from 'react-bootstrap-icons';
+import { Verse } from '@devouringscripture/common';
+import { getFormattedReference } from '@devouringscripture/refparse';
+
+interface RenderDayInterface {
+  dayNum: number;
+  maxDays: number;
+  isFreeform: boolean;
+  osis?: string;
+  verses?: Verse[];
+  incrementFunction(day: number): void;
+  decrementFunction(day: number): void;
+}
+
+export const RenderDay = ({
+  dayNum,
+  maxDays,
+  isFreeform,
+  osis,
+  verses,
+  incrementFunction,
+  decrementFunction,
+}: RenderDayInterface) => {
+  let renderedText: string = '';
+
+  if (osis !== undefined) {
+    renderedText = osis;
+  } else {
+    let tempOsis: string = '';
+    for (const verse of verses!) {
+      tempOsis += verse.osis + ',';
+      renderedText = getFormattedReference(tempOsis);
+    }
+  }
+
+  return (
+    <InputGroup>
+      <Form.Control readOnly={!isFreeform} value={renderedText} />
+      {!isFreeform ? (
+        <>
+          {dayNum > 0 ? (
+            <Button variant="outline-secondary" onClick={() => incrementFunction(dayNum)}>
+              <CaretUpFill />
+            </Button>
+          ) : (
+            <></>
+          )}
+          {dayNum < maxDays - 1 ? (
+            <Button variant="outline-secondary" onClick={() => decrementFunction(dayNum)}>
+              <CaretDownFill />
+            </Button>
+          ) : (
+            <></>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
+    </InputGroup>
+  );
+};
