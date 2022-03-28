c7f8454ad67ed529aa4b6ec7dc4037a385174e5f
sernaferna <sernaferna@gmail.com>
2022-03-28 10:13:21 -0400

    now adding days to instantiated plans

--- apps/ds-api/src/routes/instantiatedPlans/new.ts
+++ apps/ds-api/src/routes/instantiatedPlans/new.ts
@@ -5,10 +5,15 @@ import {
   validateRequest,
   BaseInstantiatedPlan,
   InstantiatedPlan,
+  InstantiatedPlanDay,
   CustomError,
   DatabaseError,
+  InvalidPlanDaysError,
 } from '@devouringscripture/common';
 import { db } from '../../services/db';
+import { DateTime } from 'luxon';
+import { getPlanById } from '../plans/byId';
+import { captureRejectionSymbol } from 'events';
 
 const router = express.Router();
 
@@ -17,18 +22,38 @@ router.post(
   [body('planInstanceId').isUUID().withMessage('Plan Instance ID required')],
   validateRequest,
   async (req: Request, res: Response, next: NextFunction) => {
-    const newBasePlan: BaseInstantiatedPlan = req.body;
+    const newBaseIP: BaseInstantiatedPlan = req.body;
     console.log(`newIP`);
 
     try {
-      const newPlan: InstantiatedPlan = {
-        planInstanceId: newBasePlan.planInstanceId,
-        percentageComplete: newBasePlan.percentageComplete,
+      const plan = getPlanById(newBaseIP.planInstanceId);
+      if (!plan.days) {
+        throw new InvalidPlanDaysError(plan.days);
+      }
+
+      const days: InstantiatedPlanDay[] = [];
+      let currentDay = DateTime.now();
+
+      for (let i = 0; i < plan.days!.length; i++) {
+        if (!plan.includeWeekends && currentDay.weekday === 6) {
+          currentDay = currentDay.plus({ days: 2 });
+        }
+        if (!plan.includeWeekends && currentDay.weekday === 7) {
+          currentDay = currentDay.plus({ days: 1 });
+        }
+        days.push({ completed: false, scheduledDate: currentDay.toISODate() });
+        currentDay = currentDay.plus({ days: 1 });
+      }
+
+      const newIP: InstantiatedPlan = {
+        planInstanceId: newBaseIP.planInstanceId,
+        percentageComplete: newBaseIP.percentageComplete,
         id: uuidv4(),
+        days: days,
       };
 
-      db.push(`/instantiatedPlans[]`, newPlan);
-      res.status(201).send(newPlan);
+      db.push(`/instantiatedPlans[]`, newIP);
+      res.status(201).send(newIP);
     } catch (err) {
       if (err instanceof CustomError) {
         return next(err);
