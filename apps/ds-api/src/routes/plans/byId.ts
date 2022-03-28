7a5f1256689e0839034abe6dc912d2be49d2ff90
sernaferna <sernaferna@gmail.com>
2022-03-28 10:13:08 -0400

    pulled get plan by id into helper function for use by other apis

--- apps/ds-api/src/routes/plans/byId.ts
+++ apps/ds-api/src/routes/plans/byId.ts
@@ -3,6 +3,29 @@ import { param } from 'express-validator';
 import { validateRequest, NotFoundError, PlanAttributes, CustomError, DatabaseError } from '@devouringscripture/common';
 import { db } from '../../services/db';
 
+/**
+ * Helper function, since other services / APIs also need plans
+ *
+ * @param id Plan Instance ID
+ * @returns Plan
+ */
+export const getPlanById = (id: string): PlanAttributes => {
+  try {
+    const index = db.getIndex('/plans', id, 'planInstanceId');
+    if (index < 0) {
+      throw new NotFoundError(`Plan ${id}`);
+    }
+    const item = db.getObject<PlanAttributes>(`/plans[${index}]`);
+    return item;
+  } catch (err) {
+    if (err instanceof CustomError) {
+      throw err;
+    } else {
+      throw new DatabaseError('getPlanById');
+    }
+  }
+};
+
 const router = express.Router();
 
 router.get(
@@ -14,12 +37,7 @@ router.get(
     console.log(`Get Plan by ID called: ${planInstanceId}`);
 
     try {
-      const index = db.getIndex('/plans', planInstanceId, 'planInstanceId');
-      if (index < 0) {
-        throw new NotFoundError(`Plan ${planInstanceId}`);
-      }
-
-      const item: PlanAttributes = db.getObject<PlanAttributes>(`/plans[${index}]`);
+      const item = getPlanById(planInstanceId);
       res.send(item);
     } catch (err) {
       if (err instanceof CustomError) {
