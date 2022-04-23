7b0ff0142ba18e8a770ce8d783a3197f33e337aa
sernaferna <sernaferna@gmail.com>
2022-04-23 10:53:53 -0400

    moved [some] toast styles out of sass and into components

---
+++ apps/ds-ui/src/components/common/toasts/ToastIcon.tsx
@@ -0,0 +1,34 @@
+import React, { useMemo } from 'react';
+import { ToastType } from './ToastManager';
+import { InfoCircleFill, ExclamationDiamondFill, CheckSquareFill } from 'react-bootstrap-icons';
+
+interface IToastIcon {
+  type: ToastType;
+  className?: string;
+}
+export const ToastIcon = ({ type, className = '' }: IToastIcon) => {
+  const theIcon = useMemo(() => {
+    switch (type) {
+      case ToastType.Info:
+        return <InfoCircleFill className={className} />;
+      case ToastType.Danger:
+        return <ExclamationDiamondFill className={className} />;
+      case ToastType.Dark:
+        return <InfoCircleFill className={className} />;
+      case ToastType.Light:
+        return <InfoCircleFill className={className} />;
+      case ToastType.Primary:
+        return <InfoCircleFill className={className} />;
+      case ToastType.Secondary:
+        return <InfoCircleFill className={className} />;
+      case ToastType.Success:
+        return <CheckSquareFill className={className} />;
+      case ToastType.Warning:
+        return <ExclamationDiamondFill className={className} />;
+      default:
+        return <InfoCircleFill className={className} />;
+    }
+  }, [type, className]);
+
+  return theIcon;
+};
