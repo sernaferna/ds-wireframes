40f593815aa041c2d2b992839e8e48c4a4a229c6
sernaferna <sernaferna@gmail.com>
2022-05-23 13:51:33 -0400

    removed show/hide prayer from Do settings

--- packages/common/src/dm/User.ts
+++ packages/common/src/dm/User.ts
@@ -16,7 +16,6 @@ export interface UserAttributes {
     showToastTester: boolean;
     actions: {
       showSettings: boolean;
-      showPrayerEntry: boolean;
     };
     home: {
       showSettings: boolean;
