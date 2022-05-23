6af9c163759a1904473494924441e1684acedf32
sernaferna <sernaferna@gmail.com>
2022-05-23 14:53:15 -0400

    put some common items onto Home and did some initial UI on it

--- apps/ds-ui/src/components/home/Home.tsx
+++ apps/ds-ui/src/components/home/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { HomeSidebar } from './HomeSidebar';
 import { Col, Row, Container } from 'react-bootstrap';
+import { CurrentReadingPlan } from '../plans/read/CurrentReadingPlan';
+import { PrayerSnapshot } from '../prayer/PrayerSnapshot';
+import { ActionsWidget } from '../do/ActionsWidget';
 
 export const Home = () => {
   return (
@@ -10,7 +13,17 @@ export const Home = () => {
           <HomeSidebar />
         </Col>
         <Col className="page-main-content-col">
-          <div>HOME</div>
+          <Row>
+            <Col xs="12" md="6" lg="4">
+              <ActionsWidget showTitle={true} />
+            </Col>
+            <Col xs="12" md="6" lg="4">
+              <CurrentReadingPlan showTitle={true} />
+            </Col>
+            <Col xs="12" lg="4">
+              <PrayerSnapshot showTitle={true} />
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
