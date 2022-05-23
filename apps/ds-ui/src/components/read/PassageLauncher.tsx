b48c78577e2c42b5c204e24b061e8adc55993b99
sernaferna <sernaferna@gmail.com>
2022-05-23 16:22:47 -0400

    using passage launcher to pop up a modal instead of just saving

--- apps/ds-ui/src/components/read/PassageLauncher.tsx
+++ apps/ds-ui/src/components/read/PassageLauncher.tsx
@@ -1,10 +1,11 @@
-import React, { useMemo, useCallback } from 'react';
+import React, { useMemo, useCallback, useState } from 'react';
 import { Col, Form, InputGroup, Row, FormControl, Button } from 'react-bootstrap';
 import { BookmarkFill, CardText } from 'react-bootstrap-icons';
 import { useNewItemMutation } from '../../services/PassagesService';
 import { BasePassage, isReferenceValid, getReferenceForOSIS, getOSISForReference } from '@devouringscripture/common';
 import * as yup from 'yup';
 import { Formik, FormikProps } from 'formik';
+import { PassageLauncherModal } from './PassageLauncherModal';
 
 const schema = yup.object({
   reference: yup
@@ -24,6 +25,7 @@ interface IPassageLauncher {
   defaultVersion: string;
 }
 export const PassageLauncher = ({ defaultVersion }: IPassageLauncher) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
   const [newItem] = useNewItemMutation();
 
   const initialValues: LauncherSchema = useMemo(
@@ -115,17 +117,32 @@ export const PassageLauncher = ({ defaultVersion }: IPassageLauncher) => {
               </Col>
               <Col xs="12" md="1">
                 <Button
-                  type="submit"
+                  type="button"
                   disabled={
                     !formikProps.touched.reference || !!formikProps.errors.version || !!formikProps.errors.reference
                   }
                   variant="primary"
                   size="sm"
+                  onClick={() => {
+                    setShowModal(true);
+                  }}
                 >
                   Go
                 </Button>
               </Col>
             </Row>
+            <PassageLauncherModal
+              show={showModal}
+              closeFunction={() => setShowModal(false)}
+              saveFunction={() => {
+                addPassage(formikProps.values.reference, formikProps.values.version);
+                setShowModal(false);
+              }}
+              passage={{
+                osis: getOSISForReference(formikProps.values.reference),
+                version: formikProps.values.reference,
+              }}
+            />
           </Form>
         )}
       </Formik>
