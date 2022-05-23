cf61730c9aeb12071a8303d948f453d193864f93
sernaferna <sernaferna@gmail.com>
2022-05-23 15:47:33 -0400

    still playing with options for content

--- apps/ds-ui/src/components/home/Home.tsx
+++ apps/ds-ui/src/components/home/Home.tsx
@@ -4,6 +4,7 @@ import { Col, Row, Container } from 'react-bootstrap';
 import { CurrentReadingPlan } from '../plans/read/CurrentReadingPlan';
 import { PrayerSnapshot } from '../prayer/PrayerSnapshot';
 import { ActionsWidget } from '../do/ActionsWidget';
+import { CreatePrayerItem } from '../prayer/CreatePrayerItem';
 
 export const Home = () => {
   return (
@@ -13,15 +14,23 @@ export const Home = () => {
           <HomeSidebar />
         </Col>
         <Col className="page-main-content-col">
+          <h1 className="d-none d-md-block">Devouring Scripture: Base Actions</h1>
           <Row>
-            <Col xs="12" md="6" lg="4">
-              <ActionsWidget showTitle={true} />
-            </Col>
             <Col xs="12" md="6" lg="4">
               <CurrentReadingPlan showTitle={true} />
             </Col>
+            <Col xs="12" md="6" lg="4">
+              <ActionsWidget showTitle={true} />
+            </Col>
             <Col xs="12" lg="4">
-              <PrayerSnapshot showTitle={true} />
+              <Row>
+                <Col xs="12" sm="4" lg="12">
+                  <PrayerSnapshot showTitle={true} />
+                </Col>
+                <Col xs="12" sm="8" lg="12">
+                  <CreatePrayerItem />
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Col>
