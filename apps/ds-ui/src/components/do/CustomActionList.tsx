fd389c670b0e133376984e0c64d84de9ce91a6ae
sernaferna <sernaferna@gmail.com>
2022-05-24 18:19:15 -0400

    added [default] option to hide some content at smaller screen sizes

--- apps/ds-ui/src/components/do/CustomActionList.tsx
+++ apps/ds-ui/src/components/do/CustomActionList.tsx
@@ -5,7 +5,7 @@ import {
   useDeleteCustomActionMutation,
 } from '../../services/ActionsService';
 import { LoadingMessage, ErrorLoadingDataMessage } from '../common/loading';
-import { Form, FloatingLabel, Button } from 'react-bootstrap';
+import { Form, FloatingLabel, Button, Alert } from 'react-bootstrap';
 import { BaseActionType, ActionType } from '@devouringscripture/common';
 
 interface IGenerateList {
@@ -42,6 +42,7 @@ export const CustomActionList = () => {
   const [actionType, setActonType] = useState('');
   const [newItem] = useNewCustomActionMutation();
   const [deleteItem] = useDeleteCustomActionMutation();
+  const [viewButtonClicked, setViewButtonClicked] = useState<boolean>(false);
 
   const setText = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
@@ -71,23 +72,30 @@ export const CustomActionList = () => {
   }
 
   return (
-    <div className="custom-action-list">
-      <h4>Custom Action List</h4>
-      <h3 className="lead">
-        Not implemented&mdash;changes here don't impact the Actions functionality in the wireframe.
-      </h3>
-      <p>You've added the following custom actions to your tracker:</p>
-      <ul>{list}</ul>
-
-      <h4>Add an Item</h4>
-      <Form onSubmit={handleSubmit}>
-        <FloatingLabel controlId="floatingItem" label="Action Type">
-          <Form.Control type="text" placeholder="Action" value={actionType} onChange={setText} />
-        </FloatingLabel>
-        <Button variant="primary" type="submit">
-          Send
+    <>
+      <Alert variant="dark" className={viewButtonClicked ? 'd-none' : 'd-block d-md-none'}>
+        <Button variant="dark" onClick={() => setViewButtonClicked(true)}>
+          Edit Custom Action List
         </Button>
-      </Form>
-    </div>
+      </Alert>
+      <div className={viewButtonClicked ? 'd-block' : 'd-none d-md-block'}>
+        <h4>Custom Action List</h4>
+        <h3 className="lead">
+          Not implemented&mdash;changes here don't impact the Actions functionality in the wireframe.
+        </h3>
+        <p>You've added the following custom actions to your tracker:</p>
+        <ul>{list}</ul>
+
+        <h4>Add an Item</h4>
+        <Form onSubmit={handleSubmit}>
+          <FloatingLabel controlId="floatingItem" label="Action Type">
+            <Form.Control type="text" placeholder="Action" value={actionType} onChange={setText} />
+          </FloatingLabel>
+          <Button variant="primary" type="submit">
+            Send
+          </Button>
+        </Form>
+      </div>
+    </>
   );
 };
