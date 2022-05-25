42816ce698ca99daca2fcd7602b85d1f4f205f09
sernaferna <sernaferna@gmail.com>
2022-05-25 13:14:35 -0400

    added to hook with additional callbacks/functions

--- apps/ds-ui/src/helpers/UserSettings.ts
+++ apps/ds-ui/src/helpers/UserSettings.ts
@@ -1,21 +1,25 @@
-import { useMemo } from 'react';
+import { useMemo, useCallback } from 'react';
 import { UserAttributes } from '@devouringscripture/common';
 import { useGetUserByIdQuery, HARDCODED_USER_ID, useUpdateUserMutation } from '../services/UserService';
 import { get as getProperty, set as setProperty } from 'lodash-es';
 
-const getNewUser = (old: UserAttributes): UserAttributes => {
-  return JSON.parse(JSON.stringify(old));
-};
-
 type FlipBoolPropertyFunction = (propPath: string) => void;
+type FlipBoolPropertyCallbackFunction = (propPath: string) => () => void;
 type UpdateStringSettingFunction = (propPath: string, newValue: string) => void;
+type UpdateStringPropertyCallbackFunction = (propPath: string, newValue: string) => () => void;
+type GetNewUserHelper = () => UserAttributes;
+type UpdateBulkUserFunction = (newUser: UserAttributes) => void;
 
 export const useUserSettings = (): [
   UserAttributes | undefined,
   any,
   boolean,
   FlipBoolPropertyFunction,
-  UpdateStringSettingFunction
+  FlipBoolPropertyCallbackFunction,
+  UpdateStringSettingFunction,
+  UpdateStringPropertyCallbackFunction,
+  GetNewUserHelper,
+  UpdateBulkUserFunction
 ] => {
   const { data, error, isLoading } = useGetUserByIdQuery(HARDCODED_USER_ID);
   const [updateUser] = useUpdateUserMutation();
@@ -30,19 +34,64 @@ export const useUserSettings = (): [
 
   const errResponse: any = useMemo(() => error, [error]);
 
-  const flipBoolCallback: FlipBoolPropertyFunction = (propPath: string) => {
-    const newUser = getNewUser(data!);
-    let newVal = getProperty(newUser, propPath) as boolean;
-    newVal = !newVal;
-    setProperty(newUser, propPath, newVal);
-    updateUser(newUser);
-  };
+  const getNewUserHelper: GetNewUserHelper = useCallback((): UserAttributes => {
+    return JSON.parse(JSON.stringify(data));
+  }, [data]);
 
-  const updateStringCallback: UpdateStringSettingFunction = (propPath: string, newValue: string) => {
-    const newUser = getNewUser(data!);
-    setProperty(newUser, propPath, newValue);
-    updateUser(newUser);
-  };
+  const updateBulkUser: UpdateBulkUserFunction = useCallback(
+    (newUser: UserAttributes) => {
+      updateUser(newUser);
+    },
+    [updateUser]
+  );
 
-  return [userObjResponse, errResponse, isLoading, flipBoolCallback, updateStringCallback];
+  const flipBool: FlipBoolPropertyFunction = useCallback(
+    (propPath: string) => {
+      const newUser = getNewUserHelper();
+      let newVal = getProperty(newUser, propPath) as boolean;
+      newVal = !newVal;
+      setProperty(newUser, propPath, newVal);
+      updateUser(newUser);
+    },
+    [updateUser, getNewUserHelper]
+  );
+
+  const flipBoolCallback: FlipBoolPropertyCallbackFunction = useCallback(
+    (propPath: string) => {
+      return () => {
+        flipBool(propPath);
+      };
+    },
+    [flipBool]
+  );
+
+  const updateString: UpdateStringSettingFunction = useCallback(
+    (propPath: string, newValue: string) => {
+      const newUser = getNewUserHelper();
+      setProperty(newUser, propPath, newValue);
+      updateUser(newUser);
+    },
+    [updateUser, getNewUserHelper]
+  );
+
+  const updateStringCallback: UpdateStringPropertyCallbackFunction = useCallback(
+    (propPath: string, newValue: string) => {
+      return () => {
+        updateString(propPath, newValue);
+      };
+    },
+    [updateString]
+  );
+
+  return [
+    userObjResponse,
+    errResponse,
+    isLoading,
+    flipBool,
+    flipBoolCallback,
+    updateString,
+    updateStringCallback,
+    getNewUserHelper,
+    updateBulkUser,
+  ];
 };
