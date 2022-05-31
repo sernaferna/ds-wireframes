69eac0b6b313b1f7c535a5ffee736c31f2a77871
sernaferna <sernaferna@gmail.com>
2022-05-31 09:54:06 -0400

    comment change

--- apps/ds-ui/src/helpers/ErrorsAndWarning.tsx
+++ apps/ds-ui/src/helpers/ErrorsAndWarning.tsx
@@ -6,7 +6,7 @@ type SetMessageFunction = (message: string | JSX.Element) => string;
 type RemoveMessageFunction = (id: string) => void;
 
 /**
- * Custom hook for working with error/warning messages. Provides dismissible
+ * Custom hook for error/warning messages. Provides dismissible
  * UI for showing the errors/warnings, and functions for
  * adding errors or warnings.
  *
