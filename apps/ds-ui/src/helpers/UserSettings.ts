246165a285fc8ebf83d68c40d6fa06ff63230f5e
sernaferna <sernaferna@gmail.com>
2022-05-31 18:36:30 -0400

    updated comment

--- apps/ds-ui/src/helpers/UserSettings.ts
+++ apps/ds-ui/src/helpers/UserSettings.ts
@@ -21,7 +21,7 @@ type UpdateBulkUserFunction = (newUser: UserAttributes) => void;
  * they'll get redrawn by React.
  *
  * The last two helpers (`GetNewUserHelper` and `UpdateBulkUserFunction`)
- * are for complicated cases.
+ * are for complicated cases where manual control is needed.
  *
  * @returns Tuple of the following items: 1) the user data (undefined until avail
  * from the API); 2) the error response (if any) from the API; 3) isLoading;
