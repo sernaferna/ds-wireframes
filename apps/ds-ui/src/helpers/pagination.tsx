c3b21cf441bf194bd7a88bae8797fd708240e053
sernaferna <sernaferna@gmail.com>
2022-06-02 13:00:34 -0400

    comment change

--- apps/ds-ui/src/helpers/pagination.tsx
+++ apps/ds-ui/src/helpers/pagination.tsx
@@ -2,7 +2,7 @@ import { Pagination } from 'react-bootstrap';
 
 /**
  * Helper function to paginate a list of items in an array (of
- * any kind). Calling component responsible for using the arrays
+ * any kind). Calling component is responsible for using the arrays
  * properly. Returns a tuple containing the subset of the array
  * that should be shown, as well as a list of Pagination.x tags
  * to be used in a <Paginate> element.
