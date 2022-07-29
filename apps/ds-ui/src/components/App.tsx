d9bc896514401bce0960217f37459b4feb9ab212
sernaferna <sernaferna@gmail.com>
2022-07-29 17:41:00 -0400

    created skeleton for admin content page

--- apps/ds-ui/src/components/App.tsx
+++ apps/ds-ui/src/components/App.tsx
@@ -13,6 +13,7 @@ import { ToastContainer } from 'react-bootstrap';
 import { PrayerPage } from './prayer/PrayerPage';
 import { PlansPage } from './plans/PlansPage';
 import { EditPlan } from './plans/edit/EditPlan';
+import { Content } from './admin/Content';
 
 export function App() {
   return (
@@ -29,6 +30,7 @@ export function App() {
             <Route key="/stats" path="/stats" element={<Stats />} />
             <Route key="/plans/edit" path="/plans/edit" element={<EditPlan />} />
             <Route key="/help" path="/help" element={<Help />} />
+            <Route key="/admin/content" path="/admin/content" element={<Content />} />
             <Route path="*" element={<FourOhFour />} />
           </Routes>
           <Footer />
