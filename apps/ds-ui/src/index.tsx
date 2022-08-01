596bfd4f0cfd8df78bba4ec9ddfda782484bc51a
sernaferna <sernaferna@gmail.com>
2022-08-01 16:21:19 -0400

    MAJOR saving data in indexedDB

--- apps/ds-ui/src/index.tsx
+++ apps/ds-ui/src/index.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom';
 import './styles/ds-styles.scss';
 import { App } from './components/App';
-import store from './stores/Store';
+import store, { persistor } from './stores/Store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- apps/ds-ui/src/stores/Store.ts
+++ apps/ds-ui/src/stores/Store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
 import uiReducer from './UISlice';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { prayerApi } from '../services/PrayerService';
@@ -9,21 +10,37 @@ import { vapiApi } from '../services/VapiService';
 import { instantiatedPlanApi } from '../services/InstantiatedPlanService';
 import { planApi } from '../services/PlanService';
 import { tutorialApi } from '../services/TutorialService';
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE, persistStore } from 'redux-persist';
+import storage from 'redux-persist-indexeddb-storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: storage('devouringScripture'),
+};
+
+const reducers = combineReducers({
+  [prayerApi.reducerPath]: prayerApi.reducer,
+  [userApi.reducerPath]: userApi.reducer,
+  [actionsApi.reducerPath]: actionsApi.reducer,
+  [passageApi.reducerPath]: passageApi.reducer,
+  [vapiApi.reducerPath]: vapiApi.reducer,
+  [instantiatedPlanApi.reducerPath]: instantiatedPlanApi.reducer,
+  [planApi.reducerPath]: planApi.reducer,
+  [tutorialApi.reducerPath]: tutorialApi.reducer,
+  ui: uiReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    [prayerApi.reducerPath]: prayerApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
-    [actionsApi.reducerPath]: actionsApi.reducer,
-    [passageApi.reducerPath]: passageApi.reducer,
-    [vapiApi.reducerPath]: vapiApi.reducer,
-    [instantiatedPlanApi.reducerPath]: instantiatedPlanApi.reducer,
-    [planApi.reducerPath]: planApi.reducer,
-    [tutorialApi.reducerPath]: tutorialApi.reducer,
-    ui: uiReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
       .concat(prayerApi.middleware)
       .concat(userApi.middleware)
       .concat(actionsApi.middleware)
@@ -37,5 +54,7 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
+
+export const persistor = persistStore(store);
 
 setupListeners(store.dispatch);
